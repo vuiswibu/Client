1e1877471c819febae46895d0e35e57ec71a414f
vuiswibu <vuiswibu@gmail.com>
2023-06-08 16:32:19 +0700

    up

---
+++ src/controller/SocketHandler.java
@@ -0,0 +1,201 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.User;
+import view.WaitingFrm;
+
+/**
+ *
+ * @author boi09
+ */
+public class SocketHandler implements Runnable{
+    private BufferedWriter os;
+    private BufferedReader is;
+    private Socket socketOfClient;
+    private int ID_Server;
+    
+    @Override
+    public void run() {
+
+        try {
+            // Gửi yêu cầu kết nối tới Server đang lắng nghe
+            socketOfClient = new Socket("127.0.0.1", 7777);
+            System.out.println("Kết nối thành công!");
+            // Tạo luồng đầu ra tại client (Gửi dữ liệu tới server)
+            os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+            // Luồng đầu vào tại Client (Nhận dữ liệu từ server).
+            is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+            String message;
+            // Bắt đầu nhận dữ liệu
+            while (true) {
+                message = is.readLine();
+                if (message == null) {
+                    break;
+                }
+                String[] messageSplit = message.split(",");
+                if(messageSplit[0].equals("server-send-id")){
+                    ID_Server = Integer.parseInt(messageSplit[1]);
+                    System.out.println(messageSplit[1]);
+                    RunClient.loginFrm.log(String.valueOf(ID_Server));
+                }
+                if(messageSplit[0].equals("login-success")){
+                    System.out.println("Đăng nhập thành công");
+                    RunClient.closeAllViews();
+                    User user= getUserFromString(1,messageSplit);
+                    RunClient.user = user;
+                    RunClient.openView(RunClient.View.HOME);
+                }
+                //Tài khoản đã đăng nhập ở nơi khác
+                if(messageSplit[0].equals("dupplicate-login")){
+                    System.out.println("Đã đăng nhập");
+                    RunClient.closeView(RunClient.View.WAITINGVERIFY);
+                    RunClient.openView(RunClient.View.LOGIN,messageSplit[1],messageSplit[2]);
+                    RunClient.loginFrm.showError("Tài khoản đã đăng nhập ở nơi khác");
+                }
+                //Xử lý register trùng tên
+                if(messageSplit[0].equals("duplicate-username")){
+                    RunClient.closeAllViews();
+                    RunClient.openView(RunClient.View.REGISTER);
+                    JOptionPane.showMessageDialog(RunClient.registeruserFrm, "Tên tài khoản đã được người khác sử dụng");
+                }
+                 //Xử lý nhận thông tin, chat từ toàn server
+//                if(messageSplit[0].equals("chat-server")){
+//                    if(RunClient.mainmenuFrm!=null){
+//                        RunClient.mainmenuFrm.addMessage(messageSplit[1]);
+//                    }
+//                }
+                //Tài khoản đã bị banned
+                if(messageSplit[0].equals("banned-user")){
+                    RunClient.closeView(RunClient.View.WAITINGVERIFY);
+                    RunClient.openView(RunClient.View.LOGIN,messageSplit[1],messageSplit[2]);
+                    RunClient.loginFrm.showError("Tài khoản đã bị ban");
+                }
+                //Đăng nhập sai mật khẩu hoặc tên đăng nhập
+                 if(messageSplit[0].equals("wrong-user")){
+                    System.out.println("Thông tin sai");
+                    RunClient.closeView(RunClient.View.WAITINGVERIFY);
+                    RunClient.openView(RunClient.View.LOGIN,messageSplit[1],messageSplit[2]);
+                    RunClient.loginFrm.showError("Tài khoản hoặc mật khẩu không chính xác");
+                }
+                if(messageSplit[0].equals("return-get-rank-charts")){
+                    if(RunClient.rankFrm!=null){
+                        RunClient.rankFrm.setDataToTable(getListRank(messageSplit));
+                    }
+                }
+                //Lấy danh sách bạn bè
+                if(messageSplit[0].equals("return-friend-list")){
+                    if(RunClient.friendFrm!=null){
+                        RunClient.friendFrm.updateFriendList(getListUser(messageSplit));
+                    }
+                }
+                //Lấy danh sách phòng
+                if(messageSplit[0].equals("room-list")){
+                    Vector<Integer> rooms = new Vector<>();
+                    //Vector<String> nplayer = new Vector<>();
+                    for(int i=1; i<messageSplit.length; i=i+2){
+                        rooms.add(Integer.parseInt(messageSplit[i]));
+//                        nplayer.add(messageSplit[i+1]);
+                    }
+                    RunClient.mainmenuFrm.updateRoomList(rooms);
+                }
+                //Tạo phòng và server trả về tên phòng
+                if(messageSplit[0].equals("your-created-room")){
+                    RunClient.closeAllViews();
+                                        System.out.println("cc");
+                    RunClient.openView(RunClient.View.CHILLROOM);
+                    RunClient.chillFrm.setRoomName(messageSplit[1]);
+                    if(messageSplit.length==3)
+                        RunClient.chillFrm.setRoomPassword("Mật khẩu phòng: "+messageSplit[2]);
+                }
+                if(messageSplit[0].equals("left-room")){
+                    //RunClient.gameClientFrm.stopTimer();
+                    RunClient.closeAllViews();
+                    RunClient.openView(RunClient.View.WAITINGVERIFY,"Đối thủ đã thoát khỏi phòng","Đang trở về trang chủ");
+                    Thread.sleep(3000);       
+                    RunClient.closeAllViews();
+                    RunClient.openView(RunClient.View.HOME);
+                }
+                if(messageSplit[0].equals("banned-notice")){
+                    RunClient.socketHandle.write("offline,"+RunClient.user.getID());
+                    RunClient.closeAllViews();
+                    RunClient.openView(RunClient.View.LOGIN);
+                    JOptionPane.showMessageDialog(RunClient.loginFrm, messageSplit[1], "Bạn đã bị BAN", JOptionPane.WARNING_MESSAGE);
+                }
+                //Xử lý cảnh cáo
+                if(messageSplit[0].equals("warning-notice")){
+                    JOptionPane.showMessageDialog(null, messageSplit[1] , "Bạn nhận được một cảnh cáo", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SocketHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public User getUserFromString(int start, String[] message){
+        return new User(Integer.parseInt(message[start]),
+                message[start+1],
+                message[start+2],
+                message[start+3],
+                message[start+4],
+                Integer.parseInt(message[start+5]),
+                Integer.parseInt(message[start+6]),
+                Integer.parseInt(message[start+7]),
+                Integer.parseInt(message[start+8]));
+    }
+    
+    
+    public List<User> getListRank(String[] message){
+        List<User> friend = new ArrayList<>();
+        for(int i=1; i<message.length; i=i+9){
+            friend.add(new User(Integer.parseInt(message[i]),
+                message[i+1],
+                message[i+2],
+                message[i+3],
+                message[i+4],
+                Integer.parseInt(message[i+5]),
+                Integer.parseInt(message[i+6]),
+                Integer.parseInt(message[i+7]),
+                Integer.parseInt(message[i+8])));
+        }
+        return friend;
+    }
+    public List<User> getListUser(String[] message){
+        List<User> friend = new ArrayList<>();
+        for(int i=1; i<message.length; i=i+4){
+            friend.add(new User(Integer.parseInt(message[i]),
+                    message[i+1],
+                    message[i+2].equals("1"),
+                    message[i+3].equals("1")));
+        }
+        return friend;
+    }
+    
+    public void write(String message) throws IOException{
+        os.write(message);
+        os.newLine();
+        os.flush();
+    }
+
+    public Socket getSocketOfClient() {
+        return socketOfClient;
+    }
+}
---
+++ src/view/RegisterUserFrm.java
@@ -0,0 +1,206 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+import controller.RunClient;
+
+public class RegisterUserFrm extends javax.swing.JFrame {
+    public RegisterUserFrm() {
+        initComponents();
+        this.setIconImage(new ImageIcon("src/assets/logoicon.png").getImage());
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        avatarComboBox1.setMaximumRowCount(5);
+        for (int i=0; i<=5; i++) {
+            avatarComboBox1.addItem(new ImageIcon("assets/avatar/"+i+".jpg"));
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jLabel6 = new javax.swing.JLabel();
+        avatarComboBox1 = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(102, 102, 102));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Register");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(196, 196, 196)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1)
+                .addContainerGap(24, Short.MAX_VALUE))
+        );
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel2.setText("Username");
+
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel3.setText("Password");
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel4.setText("NickName");
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel6.setText("Set Avatar");
+
+        jButton1.setText("Đăng kí");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setForeground(new java.awt.Color(0, 0, 255));
+        jLabel5.setText("Đã có tài khoản, đăng nhập tại đây");
+        jLabel5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel5MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(154, 154, 154)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(108, 108, 108)
+                                .addComponent(jLabel5)))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(28, 28, 28)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(avatarComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(21, Short.MAX_VALUE))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(avatarComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(26, 26, 26)
+                .addComponent(jLabel5)
+                .addGap(13, 13, 13))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        try {
+            String username = jTextField1.getText();
+            if(username.isEmpty())
+            throw new Exception("Vui lòng nhập tên tài khoản");
+            String password = String.copyValueOf(jPasswordField1.getPassword());
+            if(password.isEmpty())
+            throw new Exception("Vui lòng nhập mật khẩu");
+            String nickName = jTextField2.getText();
+            int avatar = avatarComboBox1.getSelectedIndex();
+            if(avatar==-1){
+                throw new Exception("Vui lòng chọn avatar");
+            }
+            if(nickName.isEmpty())
+            throw new Exception("Vui lòng nhập nickname");
+            RunClient.closeAllViews();
+            RunClient.openView(RunClient.View.WAITINGVERIFY, "Đăng kí tài khoản", "Đang chờ phản hồi");
+            RunClient.socketHandle.write("register,"+username+","+password+","+nickName+","+avatar);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(rootPane, ex.getMessage());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(rootPane, ex.getMessage());
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jLabel5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel5MouseClicked
+        RunClient.closeView(RunClient.View.REGISTER);
+        RunClient.openView(RunClient.View.LOGIN);
+    }//GEN-LAST:event_jLabel5MouseClicked
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<ImageIcon> avatarComboBox1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    // End of variables declaration//GEN-END:variables
+}
